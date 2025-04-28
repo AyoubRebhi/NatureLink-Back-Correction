65ac52921ece77dcb09160e7cccbdfccfdd47b08
nessmattcash <azizmehdii21@gmail.com>
2025-04-28 10:27:23 +0000

    pre final

--- src/main/java/com/example/naturelink/Repository/PackRepository.java
+++ src/main/java/com/example/naturelink/Repository/PackRepository.java
@@ -2,7 +2,23 @@ package com.example.naturelink.Repository;
 
 import com.example.naturelink.Entity.Pack;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PackRepository extends JpaRepository<Pack, Long> {
-    // Define custom query methods if needed
-}
+    @Query("SELECT p FROM Pack p LEFT JOIN FETCH p.logements WHERE p.id = :id")
+    Optional<Pack> findByIdWithLogements(@Param("id") Long id);
+
+    @Query("SELECT p FROM Pack p LEFT JOIN FETCH p.evenements WHERE p.id = :id")
+    Optional<Pack> findByIdWithEvenements(@Param("id") Long id);
+
+    @Query("SELECT p FROM Pack p LEFT JOIN FETCH p.restaurants WHERE p.id = :id")
+    Optional<Pack> findByIdWithRestaurants(@Param("id") Long id);
+
+    @Query("SELECT p FROM Pack p LEFT JOIN FETCH p.transports WHERE p.id = :id")
+    Optional<Pack> findByIdWithTransports(@Param("id") Long id);
+
+    @Query("SELECT p FROM Pack p LEFT JOIN FETCH p.activities WHERE p.id = :id")
+    Optional<Pack> findByIdWithActivities(@Param("id") Long id);}
